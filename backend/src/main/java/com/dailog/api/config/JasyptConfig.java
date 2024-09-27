9be81cf0a66e597a5c407b858579d7dbfe6f5496
Jo Myeong-geun <happy63590@gmail.com>
2024-09-28 03:49:39 +0900

    fix: 배포 오류 수정

--- backend/src/main/java/com/dailog/api/config/JasyptConfig.java
+++ backend/src/main/java/com/dailog/api/config/JasyptConfig.java
@@ -18,7 +18,7 @@ public class JasyptConfig {
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
         log.info("jasyptStringEncryptor");
-        //log.info("password: {}", password);
+        log.info("password: {}", password);
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setPassword(password);
