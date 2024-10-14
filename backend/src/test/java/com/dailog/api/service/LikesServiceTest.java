de15e8fa3604b45aa6bfa455d9649abfe0f16ecc
Jo Myeong-geun <happy63590@gmail.com>
2024-10-15 03:07:19 +0900

    test: 게시글에 좋아요를 누른 회원 조회 테스트

--- backend/src/test/java/com/dailog/api/service/LikesServiceTest.java
+++ backend/src/test/java/com/dailog/api/service/LikesServiceTest.java
@@ -11,13 +11,17 @@ import com.dailog.api.exception.likes.AlreadyLikedPost;
 import com.dailog.api.repository.LikesRepository;
 import com.dailog.api.repository.member.MemberRepository;
 import com.dailog.api.repository.post.PostRepository;
+import com.dailog.api.response.PagingResponse;
+import com.dailog.api.response.likes.LikesMemberResponse;
 import com.dailog.api.response.likes.LikesResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class LikesServiceTest {
@@ -71,7 +75,7 @@ class LikesServiceTest {
         assertThrows(AlreadyLikedPost.class, () ->
                 likesService.like(member.getEmail(), post.getId()));
     }
-    
+
     @Test
     @DisplayName("이미 누른 좋아요 취소")
     void should_CancelLike_When_AlreadyLiked() {
@@ -110,6 +114,40 @@ class LikesServiceTest {
 
         //then
         assertEquals(1L, likesResponse.getLikes());
+    }
+
+    @Test
+    @DisplayName("게시글에 좋아요를 누른 회원을 조회한다.")
+    @Transactional
+    void test() {
+        //given
+        Member member = getMember();
+        Post post = getPost(member);
+        for (int i = 1; i <= 10; i++) {
+            Member tempMember = Member.builder()
+                    .nickname(i + "번째 회원")
+                    .email("member@test.com" + i)
+                    .password("1234")
+                    .role(MEMBER)
+                    .build();
+            memberRepository.save(tempMember);
+
+            Likes likes = Likes.builder()
+                    .member(tempMember)
+                    .post(post)
+                    .build();
+            likesRepository.save(likes);
+        }
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        //when
+        PagingResponse<LikesMemberResponse> members = likesService.getMembers(post.getId(), pageRequest);
+
+        //then
+        assertEquals(10L, members.getSize());
+        assertEquals("10번째 회원", members.getItems().get(0).getNickname());
+        assertEquals("1번째 회원", members.getItems().get(9).getNickname());
     }
 
     private Member getMember() {
