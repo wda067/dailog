6d6b89146335c2f919a26a643020e94924aa85f6
Jo Myeong-geun <happy63590@gmail.com>
2024-10-22 21:48:08 +0900

    feat: Nav Bar에 주식 검색 기능 구현

--- frontend/src/layouts/TheHeader.vue
+++ frontend/src/layouts/TheHeader.vue
@@ -15,16 +15,48 @@
           <span class="navbar-toggler-icon"></span>
         </button>
         <div id="navbarSupportedContent" class="collapse navbar-collapse">
-          <ul class="navbar-nav me-auto">
+          <ul class="navbar-nav me-auto d-flex align-items-center">
             <li class="nav-item">
               <a class="nav-link" @click.prevent="goPage('PostList')"
-                >게시글 목록</a
+                >커뮤니티</a
               >
             </li>
             <li v-if="user.role === 'ADMIN'" class="nav-item">
               <a class="nav-link" @click.prevent="goPage('AdminMemberList')"
                 >회원 목록</a
               >
+            </li>
+            <li class="nav-item d-flex align-items-center ms-2">
+              <form class="d-flex position-relative" @submit.prevent>
+                <input
+                  class="form-control me-2"
+                  placeholder="주식 티커로 검색"
+                  type="text"
+                  @input="updateSearchTicker($event)"
+                  style="width: 250px; height: 40px"
+                />
+<!--                <button class="btn btn-primary" type="submit">-->
+<!--                  <i class="bi bi-search"></i>-->
+<!--                </button>-->
+                <button
+                  type="button"
+                  class="btn btn-sm"
+                  @click="clearSearch"
+                  style="position: absolute; right: 15px; height: 40px; top: 50%; transform: translateY(-50%); background: none; border: none; font-size: 1.5rem; padding: 0;"
+                >
+                  &times;
+                </button>
+              </form>
+              <ul v-if="suggestions.length != 0" class="autocomplete-list">
+                <li
+                  v-for="(suggestion, index) in suggestions"
+                  :key="index"
+                  @click="selectStock(suggestion)"
+                >
+                  <span class="stock-name">{{ truncateName(suggestion.name) }}</span><br>
+                  <span class="stock-ticker">{{ suggestion.ticker }} {{ suggestion.exchange }}</span>
+                </li>
+              </ul>
             </li>
           </ul>
 
@@ -61,7 +93,7 @@
 
 <script lang="ts" setup>
 import { useRouter } from 'vue-router';
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
 import { useAlert } from '@/composables/useAlert';
 import axios from 'axios';
 import { useAuthStore } from '@/stores/auth';
@@ -84,15 +116,79 @@ const logout = async () => {
   try {
     await axios.post('/api/auth/logout');
     authStore.cleanToken();
-    router.replace({ name: 'HomeView' });
+    await router.replace({ name: 'HomeView' });
     vSuccess('로그아웃 되었습니다.');
   } catch (error) {
     console.error('로그아웃 실패:', error);
   }
 };
 
+const searchTicker = ref('');
+const suggestions = ref<{
+  ticker: string;
+  exchange: string;
+  name: string;
+}[]>([])
+
+const updateSearchTicker = (event: Event) => {
+  searchTicker.value = (event.target as HTMLInputElement).value;
+  fetchStocks();  // 입력 값에 따라 즉시 검색을 수행
+};
+
+const fetchStocks = async () => {
+  if (searchTicker.value.length < 1) {
+    suggestions.value = [];
+    return;
+  }
+  try {
+    const response = await axios.get(`/api/stock/search`, {
+      params: { query: searchTicker.value }
+    });
+    suggestions.value = response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const response = error.response;
+      if (response?.status === 404) {
+        suggestions.value = [{ name: '검색 결과가 없습니다.', ticker: '', exchange: '' }];
+        return;
+      }
+    }
+      console.error('자동완성 데이터를 가져오는 중 오류 발생:', error);
+  }
+};
+
+const selectStock = (stock: { ticker: string; name: string; }) => {
+  searchTicker.value = stock.ticker;
+  suggestions.value = [];
+  searchTicker.value = '';
+  router.push({ name: 'StockDetail', params: { ticker: stock.ticker } });
+};
+
+const clearSearch = () => {
+  searchTicker.value = '';
+  suggestions.value = [];
+};
+
+const handleClickOutside = (event: MouseEvent) => {
+  const target = event.target as HTMLElement;
+  const formElement = document.querySelector('.position-relative');
+  if (formElement && !formElement.contains(target)) {
+    suggestions.value = []; //외부 클릭 시 suggestions 비우기
+  }
+};
+
+const truncateName = (name: string) => {
+  const maxLength = 20;
+  return name.length > maxLength ? name.slice(0, maxLength).trimEnd() + '...' : name;
+};
+
 onMounted(() => {
   authStore.initStore();
+  document.addEventListener('click', handleClickOutside);
+});
+
+onUnmounted(() => {
+  document.removeEventListener('click', handleClickOutside);
 });
 </script>
 
@@ -113,5 +209,37 @@ onMounted(() => {
 
 .profile-link:hover {
   border-bottom: 1px solid white;
+}
+
+.autocomplete-list {
+  position: absolute;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 4px; /* 모서리를 둥글게 */
+  width: 250px; /* 검색창 크기에 맞게 조정 */
+  z-index: 1000;
+  top: 100%; /* form 하단에 리스트가 뜨도록 설정 */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* 그림자 효과 추가 */
+  padding: 0;
+}
+
+.autocomplete-list li {
+  padding: 10px;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+}
+
+.autocomplete-list li:hover {
+  background-color: #f0f0f0;
+}
+
+.autocomplete-list .stock-name {
+  font-size: 16px; /* 원하는 크기로 설정 */
+  font-weight: bold;
+}
+
+.autocomplete-list .stock-ticker {
+  font-size: 13px; /* 원하는 크기로 설정 */
+  color: gray; /* 회색 설정 */
 }
 </style>
