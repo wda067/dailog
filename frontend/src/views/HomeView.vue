2ef58fc39b7b1702512241880d94a41a7d5e9b5b
Jo Myeong-geun <happy63590@gmail.com>
2024-10-22 21:49:18 +0900

    feat: HomeView에 3대 지수 차트 구현

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,19 +1,38 @@
 <template>
-  <div>
-    <h2>Home View</h2>
-    <hr class="my-4" />
-    <AppGrid v-slot="{ item }" :items="items" col-class="col-4">
-      <AppCard>{{ item }}</AppCard>
-    </AppGrid>
+  <div class="chart-container">
+    <div>
+      <h3 class="ticker-title">SPY</h3>
+      <StockChart ticker="SPY" />
+    </div>
+    <div>
+      <h3 class="ticker-title">QQQ</h3>
+      <StockChart ticker="QQQ" />
+    </div>
+    <div>
+      <h3 class="ticker-title">DIA</h3>
+      <StockChart ticker="DIA" />
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-import AppCard from '@/components/app/AppCard.vue';
-import { ref } from 'vue';
-import AppGrid from '@/components/app/AppGrid.vue';
-
-const items = ref(['a', 'b', 'c', 'd']);
+<script setup lang="ts">
+import StockChart from '@/components/stock/StockChart.vue'; // Chart 컴포넌트 임포트
 </script>
 
-<style scoped></style>
+<style scoped>
+.chart-container {
+  display: flex;
+  justify-content: space-between;
+  gap: 20px; /* 차트 간의 간격 */
+}
+
+.chart-container > div {
+  flex: 1; /* 모든 차트가 동일한 너비로 차지 */
+}
+
+.ticker-title {
+  text-align: center; /* 티커를 가운데 정렬 */
+  font-size: 1.2rem; /* 티커 제목 크기 */
+  margin-bottom: 10px; /* 티커와 차트 사이 간격 */
+}
+</style>
